f29435d3094ba9e725c7f74b0790ede68d4ead49
M K Townend <mktownend@gmail.com>
2022-03-11 00:09:11 +0000

    Initial upload

---
+++ MIDIcom4.6/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FoldersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InputToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OutputToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogTxt = New System.Windows.Forms.TextBox()
+        Me.ConvertBtn = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(540, 182)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.FoldersToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(530, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(12, 20)
+        '
+        'FoldersToolStripMenuItem
+        '
+        Me.FoldersToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InputToolStripMenuItem, Me.OutputToolStripMenuItem})
+        Me.FoldersToolStripMenuItem.Name = "FoldersToolStripMenuItem"
+        Me.FoldersToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.FoldersToolStripMenuItem.Text = "Folders"
+        '
+        'InputToolStripMenuItem
+        '
+        Me.InputToolStripMenuItem.Name = "InputToolStripMenuItem"
+        Me.InputToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.InputToolStripMenuItem.Text = "Input"
+        '
+        'OutputToolStripMenuItem
+        '
+        Me.OutputToolStripMenuItem.Name = "OutputToolStripMenuItem"
+        Me.OutputToolStripMenuItem.Size = New System.Drawing.Size(112, 22)
+        Me.OutputToolStripMenuItem.Text = "Output"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'LogTxt
+        '
+        Me.LogTxt.Location = New System.Drawing.Point(0, 206)
+        Me.LogTxt.Multiline = True
+        Me.LogTxt.Name = "LogTxt"
+        Me.LogTxt.ReadOnly = True
+        Me.LogTxt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.LogTxt.Size = New System.Drawing.Size(530, 219)
+        Me.LogTxt.TabIndex = 2
+        Me.LogTxt.WordWrap = False
+        '
+        'ConvertBtn
+        '
+        Me.ConvertBtn.Location = New System.Drawing.Point(453, 427)
+        Me.ConvertBtn.Name = "ConvertBtn"
+        Me.ConvertBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ConvertBtn.TabIndex = 3
+        Me.ConvertBtn.Text = "Anonymise"
+        Me.ConvertBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(530, 452)
+        Me.Controls.Add(Me.ConvertBtn)
+        Me.Controls.Add(Me.LogTxt)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "MIDI Study DICOM Anonymiser Tool"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents FoldersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InputToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OutputToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogTxt As TextBox
+    Friend WithEvents ConvertBtn As Button
+End Class
---
+++ MIDIcom4.6/Form1.vb
@@ -0,0 +1,162 @@
+﻿Imports System
+Imports System.IO
+Imports Dicom
+
+Public Class Form1
+
+    Dim inputFolderDlg As FolderBrowserDialog
+    Dim outputFolderDlg As FolderBrowserDialog
+
+    Dim baseDirectory As String = AppDomain.CurrentDomain.BaseDirectory()
+
+    Dim working As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not Directory.Exists(baseDirectory & "\Output\") Then
+            Directory.CreateDirectory(baseDirectory & "\Output\")
+        End If
+        If Not Directory.Exists(baseDirectory & "\Logs\") Then
+            Directory.CreateDirectory(baseDirectory & "\Logs\")
+        End If
+
+        inputFolderDlg = New FolderBrowserDialog
+        inputFolderDlg.Description = "Select input directory to convert..."
+        inputFolderDlg.SelectedPath = baseDirectory & "\Output\"
+        outputFolderDlg = New FolderBrowserDialog
+        outputFolderDlg.Description = "Select output directory for anonymised files..."
+        outputFolderDlg.SelectedPath = baseDirectory & "\Output\"
+
+    End Sub
+    Private Sub InputToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InputToolStripMenuItem.Click
+        inputFolderDlg.ShowDialog()
+    End Sub
+    Private Sub OutputToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OutputToolStripMenuItem.Click
+        outputFolderDlg.ShowDialog()
+    End Sub
+    Private Sub ConvertBtn_Click(sender As Object, e As EventArgs) Handles ConvertBtn.Click
+        If working = False Then
+            If inputFolderDlg.SelectedPath = outputFolderDlg.SelectedPath Then
+                If inputFolderDlg.ShowDialog() = DialogResult.OK AndAlso inputFolderDlg.SelectedPath <> outputFolderDlg.SelectedPath Then
+                    working = True
+                    ConvertBtn.Text = "Stop"
+                    ProcessFolder("")
+                    working = False
+                    SaveLog(DateAndTime.Now.Day & "_" & DateAndTime.Now.Month & "_" & "_" & DateAndTime.Now.Hour & "_" & DateAndTime.Now.Minute & "_" & DateAndTime.Now.Second)
+                End If
+            Else
+                working = True
+                ConvertBtn.Text = "Stop"
+                ProcessFolder("")
+                working = False
+                SaveLog(DateAndTime.Now.Day & "_" & DateAndTime.Now.Month & "_" & "_" & DateAndTime.Now.Hour & "_" & DateAndTime.Now.Minute & "_" & DateAndTime.Now.Second)
+            End If
+        Else
+            working = False
+            ConvertBtn.Text = "Anonymise"
+        End If
+    End Sub
+    Sub WriteLog(input As String)
+        LogTxt.AppendText(input & vbCrLf)
+    End Sub
+    Sub SaveLog(name As String)
+        Dim savePath As String = baseDirectory & "/Logs/" & name & ".txt"
+        If Not File.Exists(savePath) Then
+            Try
+                IO.File.WriteAllLines(savePath, LogTxt.Text.Split(vbCrLf))
+                WriteLog("***  ***  Log file saved  ***  ***")
+            Catch ex As Exception
+                WriteLog("***  ***  Log file not saved - unable to write  ***  ***")
+            End Try
+        End If
+    End Sub
+
+    Function ip(subFolder As String)
+        Return inputFolderDlg.SelectedPath & subFolder
+    End Function
+    Function op(subFolder As String)
+        Return outputFolderDlg.SelectedPath & subFolder
+    End Function
+
+    Sub ProcessFolder(folder As String)
+        If Not Directory.Exists(op(folder)) Then
+            Directory.CreateDirectory(op(folder))
+        End If
+        If Directory.Exists(ip(folder)) Then
+            For Each subFolder As String In Directory.GetDirectories(ip(folder))
+
+                If working = False Then : Exit For
+                End If
+
+                Dim folderName As String = New DirectoryInfo(subFolder).Name
+                Debug.WriteLine(folder & "\" & folderName)
+                WriteLog("Creating folder: " & folderName)
+                Application.DoEvents()
+                ProcessFolder(folder & "\" & folderName)
+
+            Next
+            For Each file As String In Directory.GetFiles(ip(folder))
+                If working = False Then : Exit For
+                End If
+                Dim fileName As String = New DirectoryInfo(file).Name
+                If fileName.ToLower.EndsWith(".dcm") Or Not fileName.Contains(".") Then
+                    Dim fileDest As String = op(folder) & "\" & fileName
+                    'IO.File.Copy(file, fileDest)
+                    WriteLog("***   Found file: " & fileName)
+                    Application.DoEvents()
+                    Try
+                        Dim dcmFile As DicomFile = DicomFile.Open(file, FileReadOption.ReadLargeOnDemand)
+
+                        dcmFile.Dataset.Remove(DicomTag.PatientAddress)
+                        dcmFile.Dataset.Remove(DicomTag.PatientAlternativeCalendar)
+                        dcmFile.Dataset.Remove(DicomTag.PatientBirthName)
+                        dcmFile.Dataset.Remove(DicomTag.PatientBirthTime)
+                        dcmFile.Dataset.Remove(DicomTag.PatientBodyMassIndex)
+                        dcmFile.Dataset.Remove(DicomTag.PatientBreedCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientBreedDescription)
+                        dcmFile.Dataset.Remove(DicomTag.PatientClinicalTrialParticipationSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientComments)
+                        dcmFile.Dataset.Remove(DicomTag.PatientDeathDateInAlternativeCalendar)
+                        dcmFile.Dataset.Remove(DicomTag.PatientEquipmentRelationshipCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientID)
+                        dcmFile.Dataset.Remove(DicomTag.PatientInstitutionResidence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientInsurancePlanCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientLocationCoordinatesCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientLocationCoordinatesSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientMotherBirthName)
+                        dcmFile.Dataset.Remove(DicomTag.PatientName)
+                        dcmFile.Dataset.Remove(DicomTag.PatientPrimaryLanguageCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientPrimaryLanguageModifierCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientReligiousPreference)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSetupLabel)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSetupNumber)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSex)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSexNeutered)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSize)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSizeCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSpeciesCodeSequence)
+                        dcmFile.Dataset.Remove(DicomTag.PatientSpeciesDescription)
+                        dcmFile.Dataset.Remove(DicomTag.PatientTelecomInformation)
+                        dcmFile.Dataset.Remove(DicomTag.PatientTelephoneNumbers)
+                        dcmFile.Dataset.Remove(DicomTag.PatientWeight)
+                        dcmFile.Dataset.Remove(DicomTag.CurrentPatientLocation)
+                        dcmFile.Dataset.Remove(DicomTag.AdditionalPatientHistory)
+                        dcmFile.Dataset.Remove(DicomTag.GroupOfPatientsIdentificationSequence)
+                        dcmFile.Dataset.Remove(DicomTag.OtherPatientIDsSequence)
+
+                        dcmFile.Dataset.AddOrUpdate(DicomTag.PatientIdentityRemoved, "YES")
+
+                        dcmFile.Save(fileDest)
+                        WriteLog("***   Anonymised file copied to: " & fileDest)
+                    Catch ex As Exception
+                        WriteLog("***   Processing failed for: " & file & " - Is this a valid DICOM file?")
+                    End Try
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("This software was developed by Dr M Townend to enable easy data collection for the MIDI Study. Please contact me on matthew.townend@wwl.nhs.uk with any issues.", "MIDI Study DICOM Anonymiser V0.1")
+    End Sub
+End Class
